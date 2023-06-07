8452b1f69e1f0e3639046f1c9926aca1972dd87f
Pankaj Kumar <pankaj.kumar@microfocus.com>
2023-06-07 22:03:53 +0530

    Maths Application

---
+++ src/main/java/MathApplication/MathApplicationTester.java
@@ -0,0 +1,36 @@
+package MathApplication;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Stack;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MathApplicationTester{
+    @InjectMocks
+    MathApplication mathApplication = new MathApplication();
+    @Mock
+    CalculatorService calculatorService;
+    @Test
+    public void passingTest(){
+        double n1 = 10.0;
+        double n2 = 20.0;
+        when(calculatorService.add(n1,n2)).thenReturn(30.0);
+        double expected = mathApplication.add(n1, n2);
+        Assert.assertEquals(expected,30.0,0);
+    }
+    @Test
+    public void failingTest(){
+        double n1 = 10.0;
+        double n2 = 20.0;
+        when(calculatorService.add(n1,n2)).thenReturn(35.0);
+        double expected = mathApplication.add(n1, n2);
+        Assert.assertNotEquals(expected,30.0,0);
+    }
+}
