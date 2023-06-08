3a08bd17b8818f12b0be9ae88502669c5502a734
Pankaj Kumar <pankaj.kumar@microfocus.com>
2023-06-08 12:11:33 +0530

    Exception handling

--- src/main/java/MathApplication/MathApplicationTester.java
+++ src/main/java/MathApplication/MathApplicationTester.java
@@ -9,10 +9,11 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Stack;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MathApplicationTester{
+
     @InjectMocks
     MathApplication mathApplication = new MathApplication();
     @Mock
@@ -21,9 +22,17 @@ public class MathApplicationTester{
     public void passingTest(){
         double n1 = 10.0;
         double n2 = 20.0;
-        when(calculatorService.add(n1,n2)).thenReturn(30.0);
+        doThrow(new RuntimeException("divide operation not implemented"))
+                .when(calculatorService).add(n1,n2);
+
+//        when(calculatorService.add(n1,n2))
+//                .thenReturn(30.0);
+
         double expected = mathApplication.add(n1, n2);
+
+
         Assert.assertEquals(expected,30.0,0);
+        verify(calculatorService,times(1)).add(n1,n2);
     }
     @Test
     public void failingTest(){
